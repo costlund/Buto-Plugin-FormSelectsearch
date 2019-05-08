d6017568e3318f1c1b66d82965a44ba2d0f818f4
Claes Ö <claes@stenbergit.se>
2019-05-08 11:58:53 +0200

    Using post instead of get.

--- public/function.js
+++ public/function.js
@@ -45,11 +45,11 @@ function PluginFormSelectsearch(){
     var element = [
       {type: 'div', innerHTML: [
           {type: 'form', innerHTML: [
-              {type: 'input', attribute: {class: 'form-control', type: 'text', id: 'sw_'+data.id}},
+              {type: 'input', attribute: {class: 'form-control', type: 'text', id: 'sw_'+data.id, name: 'sw'}},
               {type: 'input', attribute: {class: 'btn btn-default', type: 'submit', value: PluginI18nJson_v1.i18n('Search'), onclick: "PluginFormSelectsearch.search();return false;" }}
-          ], attribute: {class: 'form-inline'}}
+          ], attribute: {class: 'form-inline', id: 'form_selectsearch'}}
       ], attribute: {class: 'well'}},
-      {type: 'div', innerHTML: '', attribute: {id: 'ajax_'+data.id} } 
+      {type: 'div', innerHTML: '', attribute: {id: 'form_selectsearch_container'} } 
     ];
     PluginWfDom.render(element, document.getElementById('modal_'+data.id+'_body'));
     setTimeout(plugin_form_selectsearch_focus, 500);
@@ -61,8 +61,13 @@ function PluginFormSelectsearch(){
   this.search = function(){
     var sw = document.getElementById('sw_'+this.data.id).value.trim();
     if(sw.length >= this.sw_min_length){
-      sw = encodeURI(sw);
-      PluginWfAjax.load('ajax_'+this.data.id, this.data.url+'?sw='+sw);
+      $.post(this.data.url, $('#form_selectsearch').serialize()).done(function(data) { 
+        document.getElementById('form_selectsearch_container').innerHTML = data;
+        var scripts = document.getElementById('form_selectsearch_container').getElementsByTagName('script');
+        for (var i=0;i<scripts.length;i++) {
+          eval(scripts[i].innerHTML);
+        }
+      });
     }
   }
   /**
